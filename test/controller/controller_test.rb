721c1e74fd9414e429320eaed70a37c318f0483d
Peter Saxton <peterhsaxton@gmail.com>
2015-03-28 16:36:35 +0000

    request reponse test

--- test/controller/controller_test.rb
+++ test/controller/controller_test.rb
@@ -7,10 +7,31 @@ class InitialTest < MiniTest::Test
     @app
   end
 
+  # Integration
+
   def test_returns_404
     @app = Class.new(Sandwitch::Controller)
     get '/'
     assert_equal 404, last_response.status
+  end
+
+  # Instance methods
+
+  def test_initialises_with_app
+    controller = Sandwitch::Controller.new :app
+    assert_equal :app, controller.app
+  end
+
+  def test_can_set_request
+    controller = Sandwitch::Controller.new
+    controller.request = :request
+    assert_equal :request, controller.request
+  end
+
+  def test_can_set_response
+    controller = Sandwitch::Controller.new
+    controller.response = :response
+    assert_equal :response, controller.response
   end
 
   def test_raises_error_when_accessing_with_no_request
