770c0b0c00853be6180252e01d6994a2e31d63bc
Peter Saxton <peterhsaxton@gmail.com>
2015-04-02 12:17:43 +0100

    expand expected behaviour

--- controller_conditions_3.rb
+++ controller_conditions_3.rb
@@ -15,6 +15,29 @@ class App < Sandwitch::Controller
 
   get header(:key => /match/)
 
+  get segment(/\d+/, &:to_i) do |i|
+
+  end
+
+  post root, param('customer', :form => Cutomer::CreateForm) do |form|
+    Customer::Create.call(form, current_user) do |on|
+      on.created do |customer|
+
+      end
+      on.bad_request do
+
+      end
+      on.not_allowed do
+
+      end
+    end
+
+    usecase = Customer::Create.new(form, current_user)
+    case usecase.status
+    when :created then
+      response.status = 201
+      response['location'] = show_path(usecase.customer)
+  end
 
 
 
